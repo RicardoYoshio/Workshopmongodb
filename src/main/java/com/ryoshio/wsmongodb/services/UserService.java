37db7c0546b7cd811df8f0ad422d610477a19dd0
Ryoshio <rica.tomokane@gmail.com>
2022-01-14 13:44:14 -0300

    Insercao de usuario com Postman

--- src/main/java/com/ryoshio/wsmongodb/services/UserService.java
+++ src/main/java/com/ryoshio/wsmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ryoshio.wsmongodb.domain.User;
+import com.ryoshio.wsmongodb.dto.UserDTO;
 import com.ryoshio.wsmongodb.repository.UserRepository;
 import com.ryoshio.wsmongodb.services.exception.ObjectNotFoundException;
 
@@ -25,7 +26,14 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-		
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
 
 
