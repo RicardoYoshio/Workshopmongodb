fde71fd181cb2e708fb93d9c045e55805dffa97b
Ryoshio <rica.tomokane@gmail.com>
2022-01-18 20:22:19 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/ryoshio/wsmongodb/domain/Post.java
+++ src/main/java/com/ryoshio/wsmongodb/domain/Post.java
@@ -1,10 +1,13 @@
 package com.ryoshio.wsmongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import com.ryoshio.wsmongodb.dto.AuthorDTO;
+import com.ryoshio.wsmongodb.dto.CommentDTO;
 
 public class Post implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -14,6 +17,8 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList();
 	
 	public Post() {
 	}
@@ -68,6 +73,14 @@ public class Post implements Serializable{
 		this.author = author;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}	
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
