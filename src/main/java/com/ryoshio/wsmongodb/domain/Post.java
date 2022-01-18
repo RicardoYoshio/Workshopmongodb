a685cdf609fa337a4483da01f774d2d6fc51c1c2
Ryoshio <rica.tomokane@gmail.com>
2022-01-17 21:14:57 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/ryoshio/wsmongodb/domain/Post.java
+++ src/main/java/com/ryoshio/wsmongodb/domain/Post.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
+import com.ryoshio.wsmongodb.dto.AuthorDTO;
+
 public class Post implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
@@ -11,18 +13,19 @@ public class Post implements Serializable{
 	private Date date;
 	private String title;
 	private String body;
-	private User author;
+	private AuthorDTO author;
 	
 	public Post() {
 	}
 
-	public Post(String id, Date date, String title, String body, User author) {
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
 		this.date = date;
 		this.title = title;
 		this.body = body;
 		this.author = author;
+		
 	}
 
 	public String getId() {
@@ -57,11 +60,11 @@ public class Post implements Serializable{
 		this.body = body;
 	}
 	
-	public User getAuthor() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
 
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
 
