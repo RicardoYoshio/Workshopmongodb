313df4c957182c735ebb3dd86d0cfbefbad3eb44
Ryoshio <rica.tomokane@gmail.com>
2022-01-18 19:35:28 -0300

    Obtendo post por Id

---
+++ src/main/java/com/ryoshio/wsmongodb/services/PostService.java
@@ -0,0 +1,28 @@
+package com.ryoshio.wsmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ryoshio.wsmongodb.domain.Post;
+import com.ryoshio.wsmongodb.domain.User;
+import com.ryoshio.wsmongodb.repository.PostRepository;
+import com.ryoshio.wsmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+
+		
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}	
+	
+
+
